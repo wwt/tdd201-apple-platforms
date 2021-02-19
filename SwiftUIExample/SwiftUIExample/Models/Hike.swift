5f79f805cf603ee4e5ea2a7ac11aee05ce7c4579
Heather Meadow <heather.meadow@wwt.com>
2021-02-19 15:04:58 -0700

    SwiftUIExample - started. And we have passing tests - hm tt

---
+++ SwiftUIExample/SwiftUIExample/Models/Hike.swift
@@ -0,0 +1,8 @@
+//
+//  Hike.swift
+//  SwiftUIExample
+//
+//  Created by Heather Meadow on 2/19/21.
+//
+
+import Foundation
---
+++ SwiftUIExample/SwiftUIExampleTests/Views/CategoryViewTests.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryViewTests.swift
+//  SwiftUIExampleTests
+//
+//  Created by Heather Meadow on 2/19/21.
+//
+
+import Foundation
