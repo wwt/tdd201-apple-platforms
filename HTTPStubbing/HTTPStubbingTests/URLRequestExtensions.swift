dd0e9c54a47571d54953cad9061d44cfc8250556
Tyler Thompson <tyler.thompson@wwt.com>
2021-02-22 08:41:21 -0700

    [completed] - Slight refactor of bodyStreamAsData so that it uses defers, thanks Nick! - TT

--- HTTPStubbing/HTTPStubbingTests/URLRequestExtensions.swift
+++ HTTPStubbing/HTTPStubbingTests/URLRequestExtensions.swift
@@ -23,21 +23,19 @@ extension URLRequest {
 
     func bodySteamAsData() -> Data? {
         guard let bodyStream = self.httpBodyStream else { return nil }
-
         bodyStream.open()
+        defer { bodyStream.close() }
 
         // Will read 16 chars per iteration. Can use bigger buffer if needed
         let bufferSize: Int = 16
         let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
-        var data = Data()
+        defer { buffer.deallocate() }
 
+        var data = Data()
         while bodyStream.hasBytesAvailable {
             let readData = bodyStream.read(buffer, maxLength: bufferSize)
             data.append(buffer, count: readData)
         }
-
-        buffer.deallocate()
-        bodyStream.close()
         return data
     }
 }
