5734dc04aaab470e29a110f07e7b638d281f9ffa
Heather Meadow <heather.meadow@wwt.com>
2021-02-22 12:45:14 -0700

    SwiftUI - testing helper views - hm tt

---
+++ SwiftUIExample/SwiftUIExample/Views/Landmarks/LandmarkDetail.swift
@@ -0,0 +1,15 @@
+//
+//  LandmarkDetail.swift
+//  SwiftUIExample
+//
+//  Created by Heather Meadow on 2/22/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text("foo")
+    }
+}
