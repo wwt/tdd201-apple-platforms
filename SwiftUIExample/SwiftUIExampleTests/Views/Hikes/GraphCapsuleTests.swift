02912c76a6ba486bb95e80b454f8437bbb5ad8ce
Heather Meadow <heather.meadow@wwt.com>
2021-02-23 10:16:25 -0700

    more swift ui - hm tt

---
+++ SwiftUIExample/SwiftUIExampleTests/Views/Hikes/GraphCapsuleTests.swift
@@ -0,0 +1,27 @@
+//
+//  GraphCapsuleTests.swift
+//  SwiftUIExampleTests
+//
+//  Created by Heather Meadow on 2/23/21.
+//
+
+import Foundation
+import XCTest
+import SwiftUI
+import ViewInspector
+
+@testable import SwiftUIExample
+
+extension GraphCapsule: Inspectable { }
+
+class GraphCapsuleTests: XCTestCase {
+
+    func testGraphCapsule() throws {
+        let graphCapsule = GraphCapsule(index: 0, height: 1.0, range: 1..<20, overallRange: 3..<23)
+
+        let capsule = try graphCapsule.inspect().shape()
+
+        XCTAssertEqual(try capsule.fixedHeight(), 0.95)
+//        XCTAssertEqual(try graphCapsule.inspect().shape().offset(), CGSize(width: 1, height: 1))
+    }
+}
