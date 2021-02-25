090c181fc19bc214bb9fab0b606e8f62ded35303
Tyler Thompson <tyler.thompson@wwt.com>
2021-02-24 17:10:27 -0700

    COMPLETED - TT

--- ContractTesting/pact-node-provider/pact/provider_tests.js
+++ ContractTesting/pact-node-provider/pact/provider_tests.js
@@ -1,11 +1,11 @@
-const { Verifier } = require('@pact-foundation/pact');
-const packageJson = require('../package.json');
+const { Verifier } = require('@pact-foundation/pact')
+const packageJson = require('../package.json')
 const { app } = require('../app.js')
 const { userRepository } = require('../Repository/user-repository.js')
 
-var server = require('http').createServer(app);
+var server = require('http').createServer(app)
 server.listen(3000, () => {
-  console.log("User Service listening on http://localhost:3000")
+    console.log("User Service listening on http://localhost:3000")
 })
 
 let opts = {
@@ -16,7 +16,7 @@ let opts = {
     providerVersion: packageJson.version,
     stateHandlers: {
         [null]: () => {
-            // This is the "default" state handler, when no state is given
+            // This is the "Default" state handler, when no state is given
         },
         "A user with id 1 exists": () => {
             userRepository.clear()
@@ -26,12 +26,16 @@ let opts = {
                 age: 22,
                 email: "BobbyTheToddster@fake.com"
             })
-            return Promise.resolve(`User with ID 1 Created.`)
+            return Promise.resolve(`User with ID 1 Created`)
+        },
+        "A user with id 1 does not exist": () => {
+            userRepository.clear()
+            return Promise.resolve(`Removed all users`)
         }
     }
-};
+}
 
-new Verifier().verifyProvider(opts).then(function () {
-    console.log("Pacts successfully verified!");
+new Verifier().verifyProvider(opts).then(() => {
+    console.log("Pacts successfully verified!")
     server.close()
 });
