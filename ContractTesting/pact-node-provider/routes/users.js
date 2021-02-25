8279b298a397ccfae82a522e3ced795a877a718b
Tyler Thompson <tyler.thompson@wwt.com>
2021-02-24 17:14:24 -0700

    [contract-testing] - STARTING POINT - TT

--- ContractTesting/pact-node-provider/routes/users.js
+++ ContractTesting/pact-node-provider/routes/users.js
@@ -4,11 +4,7 @@ const { userRepository } = require('../Repository/user-repository.js')
 
 router.get('/:id', function(req, res, next) {
     const response = userRepository.getById(req.params.id)
-    if (response) {
-        res.send(response)
-    } else {
-        res.status(404).send()
-    }
+    res.status(200).send()
 });
 
 module.exports = router;
