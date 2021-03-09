646a0a6863f725bbe0835557315f96a052338957
Tyler Thompson <tyler.thompson@wwt.com>
2021-03-08 18:14:42 -0700

    [completed] - Added progressview on content view - TT

--- SwiftUIExample/SwiftUIExample/Views/Landmarks/LandmarkList.swift
+++ SwiftUIExample/SwiftUIExample/Views/Landmarks/LandmarkList.swift
@@ -6,10 +6,23 @@ A view showing a list of landmarks.
 */
 
 import SwiftUI
+import Combine
 
 struct LandmarkList: View {
     @EnvironmentObject var appModel: AppModel
+    @ObservedObject private var viewModel = ViewModel()
     @State private var showFavoritesOnly = false
+    @State private var landmarksResult: Result<[Landmark], API.HikesService.FetchHikesError>? {
+        didSet {
+            switch landmarksResult {
+                case .success(let landmarks): appModel.landmarks = landmarks
+                case .failure: showAlert = true
+                default: break
+            }
+        }
+    }
+    @State private var showAlert = false
+
     internal let inspection = Inspection<Self>()
 
     var filteredLandmarks: [Landmark] {
@@ -33,7 +46,26 @@ struct LandmarkList: View {
             }
             .navigationTitle("Landmarks")
         }
+        .onAppear {
+            viewModel.hikesService?.fetchLandmarks.map { Optional($0) }.receive(on: DispatchQueue.main)
+                .assign(to: \.landmarksResult, on: self).store(in: &viewModel.subscribers)
+        }
+        .alert(isPresented: $showAlert) {
+            let error: String = {
+                if case .failure(let err) = landmarksResult,
+                   case .apiBorked(let underlyingError) = err { return underlyingError.localizedDescription }
+                return ""
+            }()
+            return Alert(title: Text(error))
+        }
         .onReceive(inspection.notice) { self.inspection.visit(self, $0) }
+    }
+}
+
+extension LandmarkList {
+    fileprivate class ViewModel: ObservableObject {
+        @DependencyInjected var hikesService: HikesServiceProtocol?
+        var subscribers = Set<AnyCancellable>()
     }
 }
 
