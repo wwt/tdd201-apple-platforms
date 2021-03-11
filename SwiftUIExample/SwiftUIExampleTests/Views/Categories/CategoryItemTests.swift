a12070a4199ee84f8df167a16fe435317130876e
Heather Meadow <heather.meadow@wwt.com>
2021-03-11 16:23:08 -0700

    Added coverage for landmark.image .... at least what I can because SwiftUI is no bueno - hm

--- SwiftUIExample/SwiftUIExampleTests/Views/Categories/CategoryItemTests.swift
+++ SwiftUIExample/SwiftUIExampleTests/Views/Categories/CategoryItemTests.swift
@@ -16,6 +16,8 @@ extension CategoryItem: Inspectable { }
 
 class CategoryItemTests: XCTestCase {
     func testCategoryItemDisplaysCategoryNameWithLandmarks() throws {
+        let imageName = "silversalmoncreek"
+        let expectedImage = Image(imageName)
         let landmark = Landmark.createForTests(id: 1003,
                                                name: "Chilkoot Trail",
                                                park: "klondike Gold Rush National Historical Park",
@@ -24,7 +26,8 @@ class CategoryItemTests: XCTestCase {
                                                isFavorite: true,
                                                isFeatured: false,
                                                category: .mountains,
-                                               coordinates: .init(latitude: 59.560551, longitude: -135.334571))
+                                               coordinates: .init(latitude: 59.560551, longitude: -135.334571),
+                                               imageName: imageName)
         let categoryItem = CategoryItem(landmark: landmark)
         let vStack = try categoryItem.inspect().vStack()
         let name = try vStack.find(ViewType.Text.self)
@@ -32,6 +35,6 @@ class CategoryItemTests: XCTestCase {
 
         XCTAssertEqual(try name.string(), "Chilkoot Trail")
         XCTAssertEqual(try name.attributes().font(), .caption)
-        XCTAssertEqual(try image.actualImage(), landmark.image.renderingMode(.original).resizable())
+        XCTAssertEqual(try image.actualImage(), expectedImage.renderingMode(.original).resizable())
     }
 }
